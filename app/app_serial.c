a0eb788a53b26bcebdd9f25c2f696986ecdbcadf
Daniel <danielmbyerly@hotmail.com>
2023-05-18 03:08:28 -0700

    ifs modify

--- app/app_serial.c
+++ app/app_serial.c
@@ -7,7 +7,7 @@ FDCAN_FilterTypeDef CANFilter;
 uint8_t CAN_msg[CAN_DATA_LENGHT]; 
 uint8_t CAN_size;
 
-uint8_t cases;
+static uint8_t cases;
 uint8_t flag; 
 APP_MsgTypeDef td_message;  //time and date message
 
@@ -15,7 +15,7 @@ static uint8_t valid_date(uint8_t day, uint8_t month, uint8_t yearM, uint8_t yea
 static uint8_t dayofweek(uint32_t yearM, uint32_t yearL, uint32_t month, uint32_t day);
 
 /**
- * @brief   **Provide a brief fucntion description (just one line)**
+ * @brief   **Init function fot serial task(CAN init)**
  *
  *   This function provides the initialization for the CAN comunication on CAN Clasic,
  *   no prescaling is applied to the clock,the transmit queue will operate automatically,
@@ -29,11 +29,6 @@ static uint8_t dayofweek(uint32_t yearM, uint32_t yearL, uint32_t month, uint32_
  *   Sp = ( ( 11 + 1 ) / 16 ) * 100 = 75%
  *   The filter is configurate so that it only accept messages with ID 0x111
  *   The transmition is configurate with ID 0x122
- *
- * 
- *          
- *
- * 
  */
 void Serial_Init( void )
 {
@@ -53,15 +48,13 @@ void Serial_Init( void )
     CANHandler.Init.NominalTimeSeg2     = 4;
     /*setear configuracion del modulo CAN*/
     HAL_FDCAN_Init( &CANHandler );
-    
-
     /* Configure reception filter to Rx FIFO 0, este filtro solo aceptara mensajes con el ID 0x111 */
     CANFilter.IdType = FDCAN_STANDARD_ID;
     CANFilter.FilterIndex = 0;
     CANFilter.FilterType = FDCAN_FILTER_MASK;
     CANFilter.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
     CANFilter.FilterID1 = 0x111;
-    
+
     HAL_FDCAN_ConfigFilter( &CANHandler, &CANFilter );
     
     /*indicamos que los mensajes que no vengan con el filtro indicado sean rechazados*/
@@ -79,8 +72,6 @@ void Serial_Init( void )
     CANTxHeader.TxFrameType = FDCAN_DATA_FRAME;
     CANTxHeader.Identifier  = 0x122;
     CANTxHeader.DataLength  = FDCAN_DLC_BYTES_8;
-
-
 }
 
 /**
@@ -89,41 +80,27 @@ void Serial_Init( void )
  * This function Transmit the message pointed by the variable data, beore sending the message
  * we make the first value of the data pointer equal to the size pointer since this indicates
  * the size of the message that is going to be transmited. 
- * 
- *
  * @param   <*data>[in] Pointer of the array that is going to be transmited
  * @param   <*size>[in] Size of the message that is going to be transmited
- *
  * @retval  None
- *          
- *
- * 
  */
 static void CanTp_SingleFrameTx( uint8_t *data, uint8_t *size ) 
 {
-
+    CANTxHeader.DataLength  = 0x00080000U;
     *data=*size;
     HAL_FDCAN_AddMessageToTxFifoQ( &CANHandler, &CANTxHeader, data );
-    
-
 }
 
 
 /**
  * @brief   **Gets a message of the CAN communication**
- *
  *  The function first check if a message has arrive with the flag variable that is 1 when there is a message.
  *  if a message is recived the flag value is cleared.
  *  then gets the message and stores it in *data then checks if size value is greater than 1 and if a message 
  *  is valid it returns 1 if it`s not it returns 0
- * 
- *
- * @param   <*data>[in] Pointer of the array where de data is going to be store
- * @param   <*size>[in] Size of the message that`s been recived
- *
+ * @param   *data[in] Pointer of the array where de data is going to be store
+ * @param   *size[in] Size of the message that`s been recived
  * @retval  Return is 0 if data is unvalid and 1 if it is valid
- *
- * 
  */
 static uint8_t CanTp_SingleFrameRx( uint8_t *data, uint8_t *size )
 {
@@ -132,45 +109,36 @@ static uint8_t CanTp_SingleFrameRx( uint8_t *data, uint8_t *size )
     
     if(flag == TRUE){
         flag = FALSE;
-       
         
         HAL_FDCAN_GetRxMessage( &CANHandler, FDCAN_RX_FIFO0, &CANRxHeader, CAN_msg );
 
         *size=*data;
 
-        if(*size > 0u){
-
+        if(*size > 0u)
+        {
             x = TRUE;
-
         }
-        else{
-
+        else
+        {
             x = FALSE;
-
         }
     }
-    else{
+    else
+    {
         x = FALSE;
     }
-
     return x;
-
 }
 
 
 /**
  * @brief   **This is an interruption function for the CAN  **
- *
  * this function is an interruption that is called when a message is recived throught the CAN,
  * when a message is recived the flag variable is turn to 1.
- * 
- *
- * @param   <*hfdcan>[in] structure of CAN.
- * @param   <*RxFifo0ITs>[in] .
- * @param   <flag>[out] flag for new message.
+ * @param   *hfdcan[in] structure of CAN.
+ * @param   *RxFifo0ITs[in] .
+ * @param   flag[out] flag for new message.
  * @retval  None
- *
- * 
  */
 /* cppcheck-suppress misra-c2012-2.7 ; this is a library function */
 void HAL_FDCAN_RxFifo0Callback( FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs ) /* cppcheck-suppress misra-c2012-8.4 ; this is a library function */
@@ -185,16 +153,11 @@ void HAL_FDCAN_RxFifo0Callback( FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs
 
 /**
 * @brief   **Function that checks if the date is valid**
-*
 * The fucntion checks the parameters so that they are a valid date
-* 
-*
-* @param   <day>[in]       day of the month
-* @param   <month>[in]     month of the year
-* @param   <year>[in]      year of the date
+* @param   day[in]       day of the month
+* @param   month[in]     month of the year
+* @param   year[in]      year of the date
 * @retval  if the day is valid it will return 1 otherwise a 0.
-*          
-*
 * @note This is optional, just in case something very special needs to be take into account
 */
 uint8_t valid_date(uint8_t day, uint8_t month, uint8_t yearM, uint8_t yearL)
@@ -208,36 +171,42 @@ uint8_t valid_date(uint8_t day, uint8_t month, uint8_t yearM, uint8_t yearL)
 
         if ((month == JAN) || (month == MAR) || (month == MAY) || (month == JUL) || (month == AUG) || (month == OCT) || (month == DEC)) {
 
-            if (day > 31u) {
+            if (day > 31u)
+            {
                 flagd = FALSE;
             }
 
-        } else if ((month == APR) || (month == JUN) || (month == SEP) || (month == NOV)) {
-
-            if (day > 30u) {
+        } 
+        else if ((month == APR) || (month == JUN) || (month == SEP) || (month == NOV)) 
+        {
+            if (day > 30u) 
+            {
                 flagd = FALSE;
             }
 
-        } else if (month == FEB) {
-
-            if ((((year % 4u) == 0u) && ((year % 100u) != 0u)) || ((year % 400u) == 0u)) {
-
-                if (day > 29u) {
+        } 
+        else if (month == FEB) 
+        {
+            if ((((year % 4u) == 0u) && ((year % 100u) != 0u)) || ((year % 400u) == 0u)) 
+            {
+                if (day > 29u) 
+                {
                     flagd = FALSE;
                 }
 
-            } else {
-
-                if (day > 28u) {
+            } 
+            else 
+            {
+                if (day > 28u) 
+                {
                     flagd = FALSE;
                 }
-
             }
+        } else{}
 
-        }
-        else{}
-
-    } else {
+    } 
+    else 
+    {
         flagd = FALSE;
     }
 
@@ -247,18 +216,12 @@ uint8_t valid_date(uint8_t day, uint8_t month, uint8_t yearM, uint8_t yearL)
 
 /**
 * @brief   **Gets the day of the week**
-*
 * this function take the date and uses the zeller's congruence formula to get the day of the week.
-* 
-*
 * @param   <day>[in]       day of the month
 * @param   <month>[in]     month of the year
 * @param   <year>[in]      year of the date
 *
 * @retval  returns a value of the day of the week defines. 
-*          
-*
-* 
 */
 uint8_t dayofweek(uint32_t yearM, uint32_t yearL, uint32_t month, uint32_t day){
 
@@ -273,7 +236,8 @@ uint8_t dayofweek(uint32_t yearM, uint32_t yearL, uint32_t month, uint32_t day){
     uint32_t d = day;
     uint32_t w = (( ((13u * m) + 3u) / 5u) + d + y + (y / 4u) + (c / 4u) - (2u * c));
     w %= 7;
-    if (w < 0u) {
+    if (w < 0u)
+    {
         w += 7u;
     }
     return w;
@@ -293,10 +257,7 @@ uint8_t dayofweek(uint32_t yearM, uint32_t yearL, uint32_t month, uint32_t day){
 * it also calls the dayofweek function to get the day of the week if they are valid then they are store on the td_message variable 
 * an the state will be change to OK otherwise state will be FAILED
 * if the next state is SERIAL_MSG_ALARM it validates the data and if they are correct are store on the td_message variable and 
-* state is changed to ok, otherwise state will be FAILED
-* 
-*
-*          
+* state is changed to ok, otherwise state will be FAILED        
 */
 void Serial_Task( void )
 {
@@ -306,50 +267,42 @@ void Serial_Task( void )
 
             if (CanTp_SingleFrameRx(  &CAN_msg[0], &CAN_size ) == TRUE){
 
-                if(CAN_msg[1]==SERIAL_MSG_TIME){
-
+                if(CAN_msg[1]==SERIAL_MSG_TIME)
+                {
                     cases=SERIAL_MSG_TIME;
-
                 }
-                else if(CAN_msg[1]==SERIAL_MSG_DATE){
-
+                else if(CAN_msg[1]==SERIAL_MSG_DATE)
+                {
                     cases=SERIAL_MSG_DATE;
-
                 }
-                else if(CAN_msg[1]==SERIAL_MSG_ALARM){
-
+                else if(CAN_msg[1]==SERIAL_MSG_ALARM)
+                {
                     cases=SERIAL_MSG_ALARM;
-                }
+                }  else{}
+                } 
                 else{}
-
-                }
-                else{}
-            
             break;
 
        
         case SERIAL_MSG_TIME:
 
-            if( (CAN_msg[2] < 24u) && (CAN_msg[3] < 60u) && (CAN_msg[4] <60u)){
-
+            if( (CAN_msg[2] < 24u) && (CAN_msg[3] < 60u) && (CAN_msg[4] <60u))
+            {
                 td_message.tm.tm_hour=CAN_msg[2];
                 td_message.tm.tm_min=CAN_msg[3];
                 td_message.tm.tm_sec=CAN_msg[4];
                 td_message.msg=SERIAL_MSG_TIME;
-
                 cases=OK;
-
                 }
             else{
                 cases=FAILED;
                 }
-            
              break;
 
         case SERIAL_MSG_DATE:
 
-            if(valid_date(CAN_msg[2],CAN_msg[3], CAN_msg[4],CAN_msg[5]) == 1u){
-
+            if(valid_date(CAN_msg[2],CAN_msg[3], CAN_msg[4],CAN_msg[5]) == 1u)
+            {
                 td_message.tm.tm_mday = CAN_msg[2];
                 td_message.tm.tm_mon = CAN_msg[3];
                 td_message.tm.tm_year_msb = CAN_msg[4];
@@ -365,12 +318,12 @@ void Serial_Task( void )
 
         case SERIAL_MSG_ALARM:
 
-            if((CAN_msg[2] < 24u) && (CAN_msg[3] < 60u)){
+            if((CAN_msg[2] < 24u) && (CAN_msg[3] < 60u))
+            {
                 td_message.tm.tm_hour=CAN_msg[2];
                 td_message.tm.tm_min=CAN_msg[3];
                 td_message.msg = SERIAL_MSG_ALARM;
                 cases = OK;
-                
                 }
             else{
                    cases=FAILED;
