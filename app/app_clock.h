d0b2c2e3ed5ffa17d9fb715d654daa0df9c76713
Daniel <danielmbyerly@hotmail.com>
2023-05-21 22:44:42 -0700

    documentation added

--- app/app_clock.h
+++ app/app_clock.h
@@ -1,3 +1,14 @@
+/**
+* @file    <app_clock.h>
+* @brief   **Header file for app_clock.c**
+*
+*   This file contains the declaration for the functions on the .c file
+*   And also has the declarations of the variables that we need.
+*   To use this aplication you need to first use the clock init function
+*   and then you can call the Task function.
+* @note    
+*          
+*/
 #ifndef APP_CLOCK_H__
 #define APP_CLOCK_H__
 
