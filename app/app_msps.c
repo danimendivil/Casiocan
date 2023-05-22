43856a18d03e4e0681adcffb15cb384743a74227
Daniel <danielmbyerly@hotmail.com>
2023-05-21 20:51:37 -0700

    msps rtc init

--- app/app_msps.c
+++ app/app_msps.c
@@ -26,3 +26,38 @@ void HAL_FDCAN_MspInit(FDCAN_HandleTypeDef *hfdcan)  /* cppcheck-suppress misra-
     HAL_NVIC_SetPriority(TIM16_FDCAN_IT0_IRQn, 2, 0);
     HAL_NVIC_EnableIRQ(TIM16_FDCAN_IT0_IRQn);
 }
+
+/* cppcheck-suppress misra-c2012-8.4 ; this is a library function */
+void HAL_RTC_MspInit( RTC_HandleTypeDef* hrtc )   /* cppcheck-suppress misra-c2012-2.7 ; this is a library function */
+{
+    RCC_OscInitTypeDef        RCC_OscInitStruct ;
+    RCC_PeriphCLKInitTypeDef  PeriphClkInitStruct ;
+
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
+    __HAL_RCC_PWR_CLK_ENABLE();
+    
+    /*Eanlble backup domain*/
+    HAL_PWREx_ControlVoltageScaling( PWR_REGULATOR_VOLTAGE_SCALE1 );
+    HAL_PWR_EnableBkUpAccess();
+    __HAL_RCC_LSEDRIVE_CONFIG( RCC_LSEDRIVE_LOW );
+
+    /*reset previous RTC source clock*/
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+    PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_NONE;
+    HAL_RCCEx_PeriphCLKConfig( &PeriphClkInitStruct );
+    
+    /* Configure LSE/LSI as RTC clock source */
+    RCC_OscInitStruct.OscillatorType =  RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+    RCC_OscInitStruct.LSIState = RCC_LSI_OFF;
+    HAL_RCC_OscConfig( &RCC_OscInitStruct );
+
+    /*Set LSE as source clock*/
+    PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+    HAL_RCCEx_PeriphCLKConfig( &PeriphClkInitStruct );
+      
+    /* Peripheral clock enable */
+    __HAL_RCC_RTC_ENABLE();
+    __HAL_RCC_RTCAPB_CLK_ENABLE();
+}
