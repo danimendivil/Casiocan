fd06d10864491dea68bacbef431cb1d8867f0f24
Daniel <danielmbyerly@hotmail.com>
2023-05-29 01:22:02 -0700

    documentation added

--- app/hel_lcd.c
+++ app/hel_lcd.c
@@ -1,7 +1,28 @@
 #include "hel_lcd.h"
-#define     contrast_command    0x70
-#define     max_contrast        16
-void HEL_LCD_MspInit( LCD_HandleTypeDef *hlcd )
+/** 
+* @defgroup Contrast conf This values are used fot he contrast configuration
+@{ */
+#define     contrast_command    0x70u    /*!< contrast command value*/
+#define     max_contrast        16u     /*!< max value of contrast(4 bit register)*/
+/**
+@} */
+
+/** 
+* @defgroup Cursor postion values .
+@{ */
+#define     cursor_position     0x80u   /*!< cursor position command value*/
+/**
+@} */
+
+/**
+* @brief   **This function initializes the GPIO ports for the LCD**
+*
+*  This function initializate the RstPin,CsPin,RsPin and BklPin
+*  this pins has to be previusly assigned.
+*
+* @retval  none
+*/
+void HEL_LCD_MspInit( LCD_HandleTypeDef *hlcd ) /* cppcheck-suppress misra-c2012-8.7 ; function will later be used*/
 {
     HAL_Init(); 
     GPIO_InitTypeDef GPIO_InitStruct;
@@ -28,9 +49,10 @@ void HEL_LCD_MspInit( LCD_HandleTypeDef *hlcd )
 * @brief   **This function initializes the parameters for the LCD and SPI**
 *
 *  This function asignates the pins to a type LCD_HandleTypeDef structure
-*  Also Enables the pins, this function do not initialize the SPI.
+*  Also Enables the pins by calling the HEL_LCD_MspInit function, then configure
+*  The SPI and proceeds to do an initialization rutine for the LCD 
 *
-* @retval  none 
+* @retval  SPI_state[out]    State of the spi initialization
 */
 uint8_t HEL_LCD_Init( LCD_HandleTypeDef *hlcd )
 {
@@ -73,23 +95,34 @@ uint8_t HEL_LCD_Init( LCD_HandleTypeDef *hlcd )
 
     HAL_GPIO_WritePin( GPIOD, hlcd->RstPin, SET );
 
-    HEL_LCD_Command(hlcd, 0x30 ); /*wakeup*/
+    (void) HEL_LCD_Command(hlcd, 0x30 ); /*wakeup*/
 
-    HEL_LCD_Command(hlcd, 0x30 ); /*wakeup*/
-    HEL_LCD_Command(hlcd, 0x30 ); /*wakeup*/
-    HEL_LCD_Command(hlcd, 0x39 ); /*function set*/
-    HEL_LCD_Command(hlcd, 0x14 ); /*internal osc frequency*/
-    HEL_LCD_Command(hlcd, 0x56 ); /*power controll*/
-    HEL_LCD_Command(hlcd, 0x6d ); /*follower control*/
+    (void) HEL_LCD_Command(hlcd, 0x30 ); /*wakeup*/
+    (void) HEL_LCD_Command(hlcd, 0x30 ); /*wakeup*/
+    (void) HEL_LCD_Command(hlcd, 0x39 ); /*function set*/
+    (void) HEL_LCD_Command(hlcd, 0x14 ); /*internal osc frequency*/
+    (void) HEL_LCD_Command(hlcd, 0x56 ); /*power controll*/
+    (void) HEL_LCD_Command(hlcd, 0x6d ); /*follower control*/
      
-    HEL_LCD_Command(hlcd, 0x70 ); /*constrast*/
-    HEL_LCD_Command(hlcd, 0x0D ); /*display on*/
-    HEL_LCD_Command(hlcd, 0x06 ); /*entry mode*/
-    HEL_LCD_Command(hlcd, 0x01 ); /*clear screen*/
+    (void) HEL_LCD_Command(hlcd, 0x70 ); /*constrast*/
+    (void) HEL_LCD_Command(hlcd, 0x0D ); /*display on*/
+    (void) HEL_LCD_Command(hlcd, 0x06 ); /*entry mode*/
+    (void) HEL_LCD_Command(hlcd, 0x01 ); /*clear screen*/
 
     return SPI_state;
 }
 
+/**
+* @brief   **This function sends a command to the LCD**
+*
+*  This function first put the RsPin on RESET to tell the LCD that a 
+*  command is going to be recived, then puts the csPin on RESET that it knows
+*  data will be send, then we transmit the message put the Cspin
+*  on high to tell the lcd that no more data will be send
+*  and we return the status of the spi funcion
+*
+* @retval  SPI_STATUS[out]    State of the spi transmit function
+*/
 uint8_t HEL_LCD_Command( LCD_HandleTypeDef *hlcd, uint8_t cmd )
 {
     HAL_GPIO_WritePin( hlcd->RsPort, hlcd->RsPin, RESET );
@@ -103,7 +136,18 @@ uint8_t HEL_LCD_Command( LCD_HandleTypeDef *hlcd, uint8_t cmd )
     return SPI_STATUS;
 }
 
-uint8_t HEL_LCD_Data( LCD_HandleTypeDef *hlcd, uint8_t data )
+/**
+* @brief   **This function sends a Data to the LCD**
+*
+*  This function first put the RsPin on SET to tell the LCD that 
+*  data is going to be recived, then puts the csPin on RESET so that it knows
+*  data will be send, then we transmit the message put the Cspin
+*  on high to tell the lcd that no more data will be send
+*  and we return the status of the spi funcion
+*
+* @retval  SPI_STATUS[out]    State of the spi transmit function
+*/
+uint8_t HEL_LCD_Data( LCD_HandleTypeDef *hlcd, uint8_t data ) /* cppcheck-suppress misra-c2012-8.7 ; function will later be used*/
 {
     HAL_GPIO_WritePin( hlcd->RsPort, hlcd->RsPin, SET );
     HAL_GPIO_WritePin( hlcd->CsPort, hlcd->CsPin, RESET );
@@ -116,25 +160,46 @@ uint8_t HEL_LCD_Data( LCD_HandleTypeDef *hlcd, uint8_t data )
     return SPI_STATUS;
 }
 
+/**
+* @brief   **This function sends a string to the lcd**
+*
+*  This function use the strlen to see how long is the string 
+*  and then sends each byte of the string with a loop using 
+*  the HAL_LCD_Data function, if this function fails
+*  the loop is finished.
+*
+* @retval  Data_status[out]    State of the HEL_LCD_Data function
+*/
 uint8_t HEL_LCD_String( LCD_HandleTypeDef *hlcd, char *str )
 {
     uint8_t str_lenght = strlen(str);
-    uint8_t Data_status;
+    uint8_t Data_status = FALSE;
 
     for(uint8_t i = 0; i < str_lenght;i++)
     {
-        Data_status=HEL_LCD_Data(hlcd, *(str+i) );
+        Data_status=HEL_LCD_Data(hlcd, *(str+i) );  /* cppcheck-suppress misra-c2012-18.4 ; pointer operator needed*/
         if(Data_status == HAL_ERROR)
         {
-            i = str_lenght+1;
+            i = str_lenght+1u;   /* cppcheck-suppress misra-c2012-14.2 ; loop need to be stoped if condition is met*/
         }
     }
     return Data_status;
 }
 
+/**
+* @brief   **This function sets the cursor on the LCD**
+*
+*   The function asigns the variable pos the value of cursor_position
+*   wich is 0x80, thats the value of the command,
+*   set cursor in this LCD and adds the values of the position and row
+* 
+* @retval  Status for HAL_SPI_Transmit 
+*
+* @note Use the defines for row positions
+*/
 uint8_t HEL_LCD_SetCursor( LCD_HandleTypeDef *hlcd, uint8_t row, uint8_t col )
 {
-    uint8_t pos=0x80;
+    uint8_t pos = cursor_position;
     pos=(pos | row) + col;
 
     uint8_t Cursor_Status=HEL_LCD_Command(hlcd, pos );
@@ -142,23 +207,47 @@ uint8_t HEL_LCD_SetCursor( LCD_HandleTypeDef *hlcd, uint8_t row, uint8_t col )
     return Cursor_Status;
 }
 
+/**
+* @brief   **This function sets the LCD backlight**
+*
+*   The function changes the value of the pin assign.
+*   if toogle is the value then the current value is toogle
+*
+* @retval  none 
+*
+* @note Use the defines SCREEN STATE
+*/
 void HEL_LCD_Backlight( LCD_HandleTypeDef *hlcd, uint8_t state )
 {
-    if(state==TOGGLE)
+    if(state == (uint8_t)TOGGLE)
     {
-        state = !(hlcd->screen);
+        /* cppcheck-suppress misra-c2012-10.3 ; operator is needed for the function*/
+        state = !(hlcd->screen);    /* cppcheck-suppress misra-c2012-17.8 ; parameter usage leads to better code*/
     }
     HAL_GPIO_WritePin( hlcd->BklPort, hlcd->BklPin, state );
     hlcd->screen=state;
 }
 
+/**
+* @brief   **This function sets the contrast of the LCD**
+*
+*   The function asigns the variable contrast_level the value of contrast_command
+*   wich is 0x70, thats the value of the command, then checks if
+*   the value of the contrast is less than max_contrast wich is 16
+*   since thats the max value that the lcd contrast has, if it is 
+*   less then this value is added to the contrast_level and is transmited 
+*   with the HEL_LCD_Command function  
+* 
+* @retval  contrast_state Status of contrast function
+*
+*/
 uint8_t HEL_LCD_Contrast( LCD_HandleTypeDef *hlcd, uint8_t contrast )
 {
-    uint8_t contrast_state;
+    uint8_t contrast_state = FALSE;
     uint8_t contrast_level = contrast_command;
     if (contrast < max_contrast )
     {
-        HEL_LCD_Command(hlcd,(contrast_level + contrast));
+        contrast_state = HEL_LCD_Command(hlcd,(contrast_level + contrast));
     }
     else
     {
--- app/hel_lcd.h
+++ app/hel_lcd.h
@@ -4,6 +4,25 @@
     #include "app_bsp.h"
     #include <string.h>
 
+/**
+  * @brief   LCD_HandleTypedef Pins and ports of the lcd
+  */
+    typedef struct LCD_HandleTypeDef
+    {
+        SPI_HandleTypeDef   *SpiHandler; /*!< SPI handler address of the spi to use with the LCD */
+        GPIO_TypeDef        *RstPort;    /*!< Port where the pin to control the LCD reset pin is */
+        uint32_t            RstPin;      /*!< Pin to control the LCD reset pin */
+        GPIO_TypeDef        *RsPort;     /*!< Port where the pin to control the LCD RS pin is */
+        uint32_t            RsPin;       /*!< Pin to control the LCD RS pin */
+        GPIO_TypeDef        *CsPort;     /*!< Port where the pin to control the LCD chip select is */
+        uint32_t            CsPin;       /*!< Pin to control the LCD chip select pin */
+        GPIO_TypeDef        *BklPort;    /*!< Port where the pin to control the LCD backlight is */
+        uint32_t            BklPin;      /*!< Pin to control the LCD backlight pin */
+        uint8_t             screen;      /*!< State of the LCD screen */
+        // Add more elements if needed
+    } LCD_HandleTypeDef;    
+    
+    
     uint8_t HEL_LCD_Init( LCD_HandleTypeDef *hlcd );
     void HEL_LCD_MspInit( LCD_HandleTypeDef *hlcd )__attribute__((weak));
     uint8_t HEL_LCD_Command( LCD_HandleTypeDef *hlcd, uint8_t cmd );
@@ -13,21 +32,21 @@
     void HEL_LCD_Backlight( LCD_HandleTypeDef *hlcd, uint8_t state );
     uint8_t HEL_LCD_Contrast( LCD_HandleTypeDef *hlcd, uint8_t contrast );
     /** 
-    * @defgroup Row positions this are the values of the first and second row of the lcd
-    @{ */
-    #define first_row       0
-    #define second_row      0x40
+     * @defgroup Row_positions this are the values of the first and second row of the lcd
+     @{ */
+    #define first_row       0       /*!< first_row value */     /* cppcheck-suppress misra-c2012-2.5 ; tags will later be used*/
+    #define second_row      0x40    /*!< second_row value */    /* cppcheck-suppress misra-c2012-2.5 ; tags will later be used*/
     /**
-    @} */
+       @} */
 
     /** 
-    * @defgroup Screen state this are defines for the states of the screen
+    * @defgroup Screen_state this are defines for the states of the screen
     @{ */
-    #define OFF             0
-    #define ON              1
-    #define TOGGLE          2
+    #define OFF             0       /*!< OFF value */   /* cppcheck-suppress misra-c2012-2.5 ; tags will later be used*/
+    #define ON              1       /*!< ON value */     /* cppcheck-suppress misra-c2012-2.5 ; tags will later be used*/
+    #define TOGGLE          2       /*!< TOGGLE value */   /* cppcheck-suppress misra-c2012-2.5 ; tags will later be used*/
     /**
-    @} */
+        @} */
 
 
 
