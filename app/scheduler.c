6cf4440d91a8458446d8f265ddd20f762312eb1e
Daniel <danielmbyerly@hotmail.com>
2023-06-12 16:18:19 -0700

    register function added

--- app/scheduler.c
+++ app/scheduler.c
@@ -7,14 +7,15 @@
 */
 
 #include "scheduler.h"
+
 /**
 * @brief   **This function initializes the parameters for the scheduler**
 *
 *  This function initialize the taskcount to 0, also checks
 *  if the other parameters are valid, in case they`re not the program
 *  will be sent to a safe state.
-* @param   hscheduler[in] Pointer to a Scheduler_HandleTypeDef structure
-* 
+*
+* @param   hscheduler[in] Pointer to a Scheduler_HandleTypeDef structure 
 */
 void HIL_SCHEDULER_Init( Scheduler_HandleTypeDef *hscheduler )
 {
@@ -23,3 +24,38 @@ void HIL_SCHEDULER_Init( Scheduler_HandleTypeDef *hscheduler )
     assert_error( (hscheduler->tick != FALSE), SCHEDULER_ERROR );   /* cppcheck-suppress misra-c2012-11.8 ; function cannot be modify */
     hscheduler->tasksCount = 0;
 }
+
+/**
+* @brief   **This function register a task for the scheduler**
+*
+* this function sets the hscheduler with the address of the function to hold the init routine for the given task
+* and the address for the actual routine that will run as the task, plus the periodicity in milliseconds of the task to register,
+* the Periodicity should not be less than the tick value and always be multiple. 
+*
+* @param   hscheduler[in] Pointer to a Scheduler_HandleTypeDef structure 
+* @retval  Task_ID Is number from 1 to n task registered if the operation was a success, otherwise, it will return zero. 
+*/
+uint8_t HIL_SCHEDULER_RegisterTask( Scheduler_HandleTypeDef *hscheduler, void (*InitPtr)(void), void (*TaskPtr)(void), uint32_t Period )
+{
+    assert_error( (TaskPtr != NULL), SCHEDULER_ERROR ); /* cppcheck-suppress misra-c2012-11.8 ; function cannot be modify */
+    assert_error( (hscheduler->taskPtr != NULL), SCHEDULER_ERROR ); /* cppcheck-suppress misra-c2012-11.8 ; function cannot be modify */
+    assert_error( (hscheduler->tasks != FALSE), SCHEDULER_ERROR );  /* cppcheck-suppress misra-c2012-11.8 ; function cannot be modify */
+    assert_error( (hscheduler->tick != FALSE), SCHEDULER_ERROR );   /* cppcheck-suppress misra-c2012-11.8 ; function cannot be modify */
+    uint8_t Task_ID;
+    if(Period > hscheduler->tick && (Period % (hscheduler->tick)) == FALSE )
+    {
+        ((hscheduler->taskPtr)+hscheduler->tasksCount)->period = Period;
+        ((hscheduler->taskPtr)+hscheduler->tasksCount)->initFunc = InitPtr;
+        ((hscheduler->taskPtr)+hscheduler->tasksCount)->taskFunc = TaskPtr;
+        ((hscheduler->taskPtr)+hscheduler->tasksCount)->elapsed = FALSE;
+        hscheduler->tasksCount++;
+        Task_ID = hscheduler->tasksCount+1;
+    }
+    else
+    {
+        Task_ID = 0;
+    }
+
+    return Task_ID;
+}
+
