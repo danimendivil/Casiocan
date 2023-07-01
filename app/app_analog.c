4e6532efb29e0c6ea3bca313461e96ad713ef975
Daniel <danielmbyerly@hotmail.com>
2023-07-01 16:28:53 -0700

    documentation added

--- app/app_analog.c
+++ app/app_analog.c
@@ -7,6 +7,44 @@
 /**
   @} */
 
+/** 
+  * @defgroup PWM configuration values
+  @{ */
+#define PREESCALER      320    /*!< Preescaler to get 1000hz*/
+#define PERIOD          100    /*!< Period to get 1000 hz*/
+/**
+  @} */
+
+/** 
+  * @defgroup POT configuration values
+  @{ */ 
+#define MAX_CONTRAST          15u     /*!< Max value accepted by the lcd contrast command*/
+#define MAX_INTENSITY         100u    /*!< Max value of the backlight pwm*/
+#define MAX_POT_VALUE         255u    /*!< Max value of the pot*/
+/**
+  @} */
+
+/** 
+  * @defgroup TIM_ADC configuration values
+  @{ */
+#define PREESCALER_ADC_TIM      10000      /*!< Preescaler to get 50ms*/
+#define PERIOD_ADC_TIM          64         /*!< Period to get 50ms*/
+/**
+  @} */
+
+/**
+  * @defgroup Numbers defines
+  * @{
+  */
+#define CERO        0u    /*!< Value for counter: 0 */
+#define ONE         1u    /*!< Value for counter: 1 */
+#define TWO         2u    /*!< Value for counter: 2 */
+#define THREE       3u    /*!< Value for counter: 3 */
+#define FOUR        4u    /*!< Value for counter: 4 */
+/**
+  * @}
+  */
+
 /**
  * @brief  Variable for tim used by the pwm
  */
@@ -15,7 +53,7 @@ static TIM_HandleTypeDef TimHandle2;            /*TIM initial structure*/
 /**
  * @brief  Array were data of the DMA will be stored
  */
-static uint32_t AdcData[3u];
+static uint32_t AdcData[ARRAY_LENGHT];
 
 /**
  * @brief  Variable DMA configuration
@@ -32,11 +70,21 @@ static uint8_t Analogs_GetContrast( void );
 static uint8_t Analogs_GetIntensity( void );
 
 /**
- * @brief   **This function intiates the functions used by the analog file**
- *
- *  In this function first we initialize the TIM3
- *  
- */
+* @brief   **This function intiates the functions used by the analog file**
+*
+*  In this function first we initialize the TIM3 wich will be the tim in control of the pwm
+*  for the lcd backlight pin to adjust the intensity level, this pwm is set to 1khz: 
+*  pwm_frequency=32mhz/320/100=1000hz.
+*  then we initialized the DMA to be linked with the ADC1, then we initialized a tim 
+*  so that the timer will automatically trigger the ADC conversion at each interval,
+*  without any CPU intervention, this timer is every 20ms
+*  tim_frequency=32mhz/64/10000=50ms.
+*  then we confidgure the adc parameters, the resolution will be of 8 bits, it is 
+*  configure to be triggered by the tim and to call the dma to store the data.
+*  lastly we configure the channels to be read by the ADC wich are 2 pots
+*  and the internal temperature sensor.
+* 
+*/
 void Analogs_Init( void )
 {
   
@@ -52,15 +100,15 @@ void Analogs_Init( void )
   __TIM4_CLK_ENABLE(); 
 
   TimHandle2.Instance = TIM3;
-  TimHandle2.Init.Prescaler     = 3200;
-  TimHandle2.Init.Period        = 1000;
+  TimHandle2.Init.Prescaler     = PREESCALER;
+  TimHandle2.Init.Period        = PERIOD;
   TimHandle2.Init.CounterMode   = TIM_COUNTERMODE_UP;
   HAL_TIM_PWM_Init(&TimHandle2);
   
   sConfig.OCMode     = TIM_OCMODE_PWM1;
   sConfig.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfig.OCFastMode = TIM_OCFAST_DISABLE;
-  sConfig.Pulse = 100;
+
   HAL_TIM_PWM_ConfigChannel( &TimHandle2, &sConfig, TIM_CHANNEL_1 );
   HAL_TIM_PWM_Start( &TimHandle2, TIM_CHANNEL_1 );
 
@@ -80,11 +128,11 @@ void Analogs_Init( void )
   
   /*configure timer 4 to generate a 1 seg (16MHz) timebase*/
   TimHandler.Instance               = TIM4;
-  TimHandler.Init.Period            = 64;        /*value for 50ms*/
-  TimHandler.Init.Prescaler         = 10000;
-  TimHandler.Init.ClockDivision     = 0;
+  TimHandler.Init.Period            = PERIOD_ADC_TIM;        
+  TimHandler.Init.Prescaler         = PREESCALER_ADC_TIM;
+  TimHandler.Init.ClockDivision     = FALSE;
   TimHandler.Init.CounterMode       = TIM_COUNTERMODE_UP;
-  TimHandler.Init.RepetitionCounter = 0x0;
+  TimHandler.Init.RepetitionCounter = FALSE;
   HAL_TIM_Base_Init( &TimHandler );
   
   /* Config timer to generate an internal trigger signal */
@@ -93,89 +141,119 @@ void Analogs_Init( void )
   HAL_TIMEx_MasterConfigSynchronization( &TimHandler, &sMasterConfig );
 
   /*enable DMA interrupts*/
-  HAL_NVIC_SetPriority( DMA1_Channel1_IRQn, 2, 0 );
+  HAL_NVIC_SetPriority( DMA1_Channel1_IRQn, TWO, CERO );
   HAL_NVIC_EnableIRQ( DMA1_Channel1_IRQn );
 
-  /* Conversion time is given by::
-  Tcon = ( Tsampling + Tconv ) / ADC clock 
-  Tcon = ( AdcHandler.Init.SamplingTimeCommon1 + AdcHandler.Init.Resolution ) / ( APB Clock / AdcHandler.Init.ClockPrescaler )
-  Tcon = ( 160.5 + 8.5 ) / 2MHz = 84.5us */
   AdcHandler.Instance                   = ADC1;
   AdcHandler.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV4;   /*APB clock divided by two*/
   AdcHandler.Init.Resolution            = ADC_RESOLUTION8b;           /*8 bit resolution with a Tconv of 8.5*/
   AdcHandler.Init.ScanConvMode          = ADC_SCAN_SEQ_FIXED;         /*scan adc channels from 0 to 16 in that order*/
   AdcHandler.Init.DataAlign             = ADC_DATAALIGN_RIGHT;        /*data converter is right alightned*/
-  AdcHandler.Init.SamplingTimeCommon1   = ADC_SAMPLETIME_79CYCLES_5;    /*sampling time of 1.5*/  
+  AdcHandler.Init.SamplingTimeCommon1   = ADC_SAMPLETIME_39CYCLES_5;    /*sampling time of 1.5*/  
   AdcHandler.Init.ExternalTrigConv      = ADC_EXTERNALTRIG_T4_TRGO;   /*set the timer TIM3 to trigger the ADC*/
   AdcHandler.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_RISING;  /*only on rising edges*/
   AdcHandler.Init.DMAContinuousRequests = ENABLE;
   AdcHandler.Init.EOCSelection          = ADC_EOC_SEQ_CONV;        /*ISR at the end of one channel conversion*/
   AdcHandler.Init.Overrun               = ADC_OVR_DATA_OVERWRITTEN;   /*data will be overwriten in case is not read it*/
-  AdcHandler.Init.OversamplingMode      = 4;   
+  AdcHandler.Init.OversamplingMode      = FOUR;   
   HAL_ADC_Init( &AdcHandler );
-  /*apply ADC configuration*/
   
-      /*config adc channel number 0*/
   sChanConfig.Channel = ADC_CHANNEL_0;
   sChanConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
   sChanConfig.SamplingTime = ADC_SAMPLINGTIME_COMMON_1;
-  /*apply channel configuration*/
   HAL_ADC_ConfigChannel( &AdcHandler, &sChanConfig );
 
-  /*config adc channel number 0*/
+
   sChanConfig.Channel = ADC_CHANNEL_1;
   sChanConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
   sChanConfig.SamplingTime = ADC_SAMPLINGTIME_COMMON_1;
-  /*apply channel configuration*/
   HAL_ADC_ConfigChannel( &AdcHandler, &sChanConfig );
   
-
-  /*config adc channel number 1*/
   sChanConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
   sChanConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
   sChanConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-  /*apply channel configuration*/
   HAL_ADC_ConfigChannel( &AdcHandler, &sChanConfig );
 
-  /*Apply internal calibration*/
+  
   HAL_ADCEx_Calibration_Start( &AdcHandler );
 
-  HAL_ADC_Start_DMA( &AdcHandler, &AdcData[0], 3u );
+  HAL_ADC_Start_DMA( &AdcHandler, &AdcData[CERO], THREE );
 
   HAL_TIM_Base_Start( &TimHandler );
 }
 
+/**
+* @brief   **This returns the temperature**
+*
+*  this function returns the temperature given by the internal sensor
+*  this uses the hal function __HAL_ADC_CALC_TEMPERATURE this takes as parameter
+*  the vref of the sensor in wich we will use the define VDD_VALUE that is equal to
+*  3300 note that this value refers to milivolts, the value of the temperature
+*  that is stored by the dma in the position 2 of Adcdata array
+*  
+* @retval  temperature temperature of the sensor on C
+*/
 int8_t Analogs_GetTemperature( void )
 {
   int8_t temperature;
 
-  temperature = __HAL_ADC_CALC_TEMPERATURE( VDD_VALUE, AdcData[2], ADC_RESOLUTION8b);
+  temperature = __HAL_ADC_CALC_TEMPERATURE( VDD_VALUE, AdcData[TWO], ADC_RESOLUTION8b);
 
   return temperature;
 }
 
+/**
+* @brief   **This returns the contrast of the lcd**
+*
+*  this function returns the contrast of the lcd, this is given
+*  by the pot value stored on AdcData[1], and we do a convertion
+*  so that the value will be betwen 0 and 15;
+*  
+* @retval  contrast contrast of the sensor
+*/
 uint8_t Analogs_GetContrast( void )
 {
   uint8_t contrast;
 
-  contrast = (AdcData[1]*15u)/255u;
+  contrast = (AdcData[1]*MAX_CONTRAST)/MAX_POT_VALUE;
 
   return contrast;
 }
 
+/**
+* @brief   **This returns the intensity of the lcd**
+*
+*  this function returns the intensity of the lcd, this is given
+*  by the pot value stored on AdcData[0], and we do a convertion
+*  so that the value will be betwen 0 and 100;
+*  
+* @retval  intensity intensity of the sensor
+*/
 uint8_t Analogs_GetIntensity( void )
 {
   uint8_t intensity;
 
-  intensity = (AdcData[0]*100u)/255u;
+  intensity = (AdcData[CERO]*MAX_INTENSITY)/MAX_POT_VALUE;
 
   return intensity;
 }
 
+/**
+* @brief   **This function applied the of intensity and contrast to the lcd**
+*
+*  the function first checks if the value read by the pot is different to the one
+*  that was previously applied, in both cases if its different then it applies the value
+*  in the case of the intensity level this is applied by changing the pwm period of the 
+*  lcd backlight.
+*  in the case of the contrast the value changes by calling the function HEL_LCD_Contrast
+*  wich sends a command to the lcd to change the contrast, the second parameter is the contrast value
+*  we use the previous fucntion to get this values.
+*  
+*/
 void Display_LcdTask( void )
 {
-  static uint8_t intensity_level   = 0;
-  static uint8_t contrast_level    = 0;
+  static uint8_t intensity_level   = FALSE;
+  static uint8_t contrast_level    = FALSE;
   
   if(intensity_level !=Analogs_GetIntensity())
   {
